e65b41b9467c316cb7be7129d32a58aff9f7de33
Szymon Zak <szymonszmykzak@gmail.com>
2019-01-02 17:09:26 +0100

    Update copyrights

--- src/gmbt/Properties/AssemblyInfo.cs
+++ src/gmbt/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyDescription("Simple tool designed to help in testing and building Gothic and Gothic 2 Night of the Raven mods.")]
 [assembly: AssemblyCompany("Szymon Zak")]
 [assembly: AssemblyProduct("Gothic Mod Build Tool")]
-[assembly: AssemblyCopyright("Copyright (c) Szymon Zak 2018")]
+[assembly: AssemblyCopyright("Copyright (c) Szymon Zak 2019")]
 
 [assembly: ComVisible(false)]
 
